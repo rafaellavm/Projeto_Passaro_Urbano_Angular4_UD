555aeb6eafa2ef82da2cd4ad3746c93d10d9d6a2
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-06-18 17:47:53 -0300

    116) Recuperando ofertas do tipo Restaurantes (HTTP Request/ API rest/ Promise)

--- app2/src/app/restaurantes/restaurantes.component.ts
+++ app2/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService} from '../ofertas.service';
 
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [OfertasService] //servicos que serao utilizados
 })
+
 export class RestaurantesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit() {
+
+    this.ofertaService.getOfertasPorCategoria('restaurante')
+    .then((ofertas: Oferta[]) =>{
+      console.log("Categoria restaurante = " , ofertas);
+    })
   }
 
 }
